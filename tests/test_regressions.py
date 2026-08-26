8007b982b61c4cabeddfb0486c94c658f777e44e
Xinyuan Li <xinyuanli2327@outlook.com>
2026-08-26 14:32:42 +0800

    fix(g1): standardize the free-base joint name

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -10,3 +10,9 @@ def test_kitchen_scenes_compile_with_mujoco_33():
     for scene in ("g1_torque_kitchen.xml", "h1hand_pos_kitchen.xml"):
         model = mujoco.MjModel.from_xml_path(str(ASSET_ROOT / "envs" / scene))
         assert model.nq > 0
+
+
+def test_g1_uses_the_free_base_name_expected_by_tasks():
+    for scene in ("g1_torque_truck.xml", "g1_torque_run.xml"):
+        model = mujoco.MjModel.from_xml_path(str(ASSET_ROOT / "envs" / scene))
+        assert model.joint("free_base").id == 0
