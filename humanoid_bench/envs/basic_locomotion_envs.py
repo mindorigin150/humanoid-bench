82cbb70fc18882175db03dc9ff4f013c93d0b5e0
Xinyuan Li <xinyuanli2327@outlook.com>
2026-08-27 22:34:29 +0800

    fix(humanoid-bench): define custom hurdle reward and contact contract

--- humanoid_bench/envs/basic_locomotion_envs.py
+++ humanoid_bench/envs/basic_locomotion_envs.py
@@ -226,71 +226,214 @@ class Slide(ClimbingUpwards):
 
 
 class Hurdle(Walk):
-    _move_speed = _RUN_SPEED
     camera_name = "cam_hurdle"
+    _hurdle_x_positions = np.arange(7.0, 71.0, 7.0)
+    _hurdle_clearance = 3.0
+    _hurdle_geom_names = tuple(
+        f"hurdle_{hurdle_index}_collision" for hurdle_index in range(1, 11)
+    )
+
+    def __init__(
+        self,
+        robot=None,
+        env=None,
+        *,
+        hurdle_count=10,
+        hurdle_horizon=1000,
+        **kwargs,
+    ):
+        super().__init__(robot, env, **kwargs)
+        self._hurdle_count = hurdle_count
+        self._hurdle_horizon = hurdle_horizon
+        self._passed_hurdle_ids = set()
+        self._hurdle_contact_ids = set()
+        self._falls_or_nonfoot_ground_contact = False
+        self._previous_root_x = None
+        self._episode_steps = 0
+
+        if env is not None:
+            self._hurdle_geom_ids = tuple(
+                env.model.geom(name).id for name in self._hurdle_geom_names
+            )
+            self._floor_geom_id = env.model.geom("floor").id
+            foot_body_ids = tuple(
+                env.model.site_bodyid[env.model.site(site_name).id]
+                for site_name in ("left_foot", "right_foot")
+            )
+            self._foot_geom_ids = tuple(
+                np.flatnonzero(np.isin(env.model.geom_bodyid, foot_body_ids))
+            )
+
+    def reset_model(self):
+        self._passed_hurdle_ids = set()
+        self._hurdle_contact_ids = set()
+        self._falls_or_nonfoot_ground_contact = False
+        self._previous_root_x = self._env.data.qpos[0]
+        self._episode_steps = 0
+        return super().reset_model()
+
+    @property
+    def observation_space(self):
+        return Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.robot.dof * 2,),
+            dtype=np.float64,
+        )
+
+    def get_obs(self):
+        state = super().get_obs()
+        self._record_contacts()
+        prospective_passed_ids = (
+            self._passed_hurdle_ids | self._newly_cleared_hurdle_ids()
+        )
+        terminal = (
+            len(prospective_passed_ids) == self._hurdle_count
+            or bool(self._hurdle_contact_ids)
+            or self._falls_or_nonfoot_ground_contact
+        )
+        if terminal:
+            next_hurdle_distance = 0.0
+        else:
+            next_hurdle_index = next(
+                index
+                for index, geom_id in enumerate(
+                    self._hurdle_geom_ids[: self._hurdle_count]
+                )
+                if geom_id not in prospective_passed_ids
+            )
+            next_hurdle_distance = max(
+                self._hurdle_x_positions[next_hurdle_index]
+                - self._env.data.qpos[0],
+                0.0,
+            )
+        return np.concatenate((state, [next_hurdle_distance]))
+
+    def _newly_cleared_hurdle_ids(self):
+        root_x = self._env.data.qpos[0]
+        return {
+            geom_id
+            for hurdle_x, geom_id in zip(
+                self._hurdle_x_positions[: self._hurdle_count],
+                self._hurdle_geom_ids[: self._hurdle_count],
+            )
+            if self._previous_root_x < hurdle_x + self._hurdle_clearance <= root_x
+            and geom_id not in self._passed_hurdle_ids
+            and geom_id not in self._hurdle_contact_ids
+        }
+
+    def _current_contacts(self):
+        current_hurdle_contact_ids = set()
+        nonfoot_ground_contact = False
+        for pair in self._env.data.contact.geom:
+            pair_ids = set(pair)
+            current_hurdle_contact_ids.update(
+                pair_ids.intersection(self._hurdle_geom_ids[: self._hurdle_count])
+            )
+            if self._floor_geom_id in pair_ids and not pair_ids.intersection(
+                self._foot_geom_ids
+            ):
+                nonfoot_ground_contact = True
+        return current_hurdle_contact_ids, nonfoot_ground_contact
+
+    def _record_contacts(self):
+        current_hurdle_contact_ids, nonfoot_ground_contact = self._current_contacts()
+        self._hurdle_contact_ids.update(current_hurdle_contact_ids)
+        self._falls_or_nonfoot_ground_contact = (
+            self._falls_or_nonfoot_ground_contact
+            or self._env.data.qpos[2] < 0.2
+            or nonfoot_ground_contact
+        )
 
     def get_reward(self):
-        self.wall_collision_ids = [
-            self._env.named.data.geom_xpos.axes.row.names.index(wall_name)
-            for wall_name in [
-                "left_barrier_collision",
-                "right_barrier_collision",
-                "behind_barrier_collision",
-            ]
-        ]
+        self._episode_steps += 1
+        self._record_contacts()
 
-        standing = rewards.tolerance(
-            self.robot.head_height(),
-            bounds=(_STAND_HEIGHT, float("inf")),
-            margin=_STAND_HEIGHT / 4,
-        )
-        upright = rewards.tolerance(
-            self.robot.torso_upright(),
-            bounds=(0.8, float("inf")),
-            sigmoid="linear",
-            margin=1.9,
-            value_at_margin=0,
-        )
-        stand_reward = standing * upright
-        small_control = rewards.tolerance(
-            self.robot.actuator_forces(),
-            margin=10,
-            value_at_margin=0,
-            sigmoid="quadratic",
-        ).mean()
-        small_control = (4 + small_control) / 5
-        com_velocity = self.robot.center_of_mass_velocity()[0]
-        move = rewards.tolerance(
-            com_velocity,
-            bounds=(self._move_speed, float("inf")),
-            margin=self._move_speed,
-            value_at_margin=0,
-            sigmoid="linear",
-        )
-        move = (5 * move + 1) / 6
-        wall_collision_discount = 1
+        root_x = self._env.data.qpos[0]
+        previous_root_x = self._previous_root_x
+        previous_hurdles_passed = len(self._passed_hurdle_ids)
+        self._passed_hurdle_ids.update(self._newly_cleared_hurdle_ids())
+        self._previous_root_x = root_x
 
-        for pair in self._env.data.contact.geom:
-            if any(
-                [
-                    wall_collision_id in pair
-                    for wall_collision_id in self.wall_collision_ids
-                ]
-            ):  # for no hand. if for hand, > 155
-                wall_collision_discount = 0.1
-                # print(pair)
-                break
+        progress = root_x - previous_root_x
+        hurdles_passed = len(self._passed_hurdle_ids)
+        new_hurdles_passed = hurdles_passed - previous_hurdles_passed
+        course_complete = hurdles_passed == self._hurdle_count
 
-        reward = small_control * stand_reward * move * wall_collision_discount
+        root_y = self._env.data.qpos[1]
+        w, x, y, z = self._env.data.qpos[3:7]
+        yaw = np.arctan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
+        teacher_accepted = bool(
+            course_complete
+            and not self._hurdle_contact_ids
+            and not self._falls_or_nonfoot_ground_contact
+            and abs(root_y) <= 0.5
+            and abs(yaw) <= np.deg2rad(5.0)
+        )
+        hurdle_contact = bool(self._hurdle_contact_ids)
+        fall_or_nonfoot_ground_contact = self._falls_or_nonfoot_ground_contact
+        physical_failure = hurdle_contact or fall_or_nonfoot_ground_contact
+        quality_failure = (
+            course_complete and not teacher_accepted and not physical_failure
+        )
+        timeout = self._episode_steps >= self._hurdle_horizon
+        forward_quality = (
+            np.clip(progress, 0.0, 0.05) / 0.05
+            * np.exp(-2.0 * root_y**2 - yaw**2)
+        )
+        step_reward = 0.02 * forward_quality
+        hurdle_pass_reward = 5.0 * new_hurdles_passed * (
+            not physical_failure and (not course_complete or teacher_accepted)
+        )
+        course_complete_reward = 200.0 * teacher_accepted
+        failure_penalty = -200.0 * physical_failure
+        quality_failure_penalty = -200.0 * quality_failure
+        timeout_penalty = -200.0 * (timeout and not course_complete)
+        if physical_failure:
+            reward = failure_penalty
+        elif teacher_accepted:
+            reward = step_reward + hurdle_pass_reward + course_complete_reward
+        elif quality_failure:
+            reward = quality_failure_penalty
+        elif timeout:
+            reward = timeout_penalty
+        else:
+            reward = step_reward + hurdle_pass_reward
 
         return reward, {
-            "stand_reward": stand_reward,
-            "small_control": small_control,
-            "move": move,
-            "standing": standing,
-            "upright": upright,
-            "wall_collision_discount": wall_collision_discount,
+            "progress": progress,
+            "forward_quality": forward_quality,
+            "step_reward": step_reward,
+            "new_hurdles_passed": new_hurdles_passed,
+            "hurdle_pass_reward": hurdle_pass_reward,
+            "course_complete_reward": course_complete_reward,
+            "failure_penalty": failure_penalty,
+            "quality_failure_penalty": quality_failure_penalty,
+            "timeout_penalty": timeout_penalty,
+            "root_y": root_y,
+            "yaw": yaw,
+            "hurdles_passed": hurdles_passed,
+            "first_hurdle_clean_pass": self._hurdle_geom_ids[0]
+                in self._passed_hurdle_ids,
+            "hurdle_contacts": len(self._hurdle_contact_ids),
+            "falls_or_nonfoot_ground_contact": self._falls_or_nonfoot_ground_contact,
+            "course_complete": course_complete,
+            "teacher_accepted": teacher_accepted,
+            "timeout": timeout,
+            "hurdle_horizon": self._hurdle_horizon,
         }
+
+    def get_terminated(self):
+        self._record_contacts()
+        return (
+            bool(
+                self._hurdle_contact_ids
+                or self._falls_or_nonfoot_ground_contact
+                or len(self._passed_hurdle_ids) == self._hurdle_count
+                or self._episode_steps >= self._hurdle_horizon
+            ),
+            {"timeout": self._episode_steps >= self._hurdle_horizon},
+        )
 
 
 class Sit(Task):
